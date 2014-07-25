7a00ea8f894ceb9502fa233551c74591b93316ff
leifeiyatan <601137669@qq.com>
2014-07-25 16:19:05 +0800

    testChange

--- Classes/GameLayer/GamePlayLayer.cpp
+++ Classes/GameLayer/GamePlayLayer.cpp
@@ -83,22 +83,6 @@ void GamePlayLayer::LayoutUnit()
 
     log("%s\n", Director::getInstance() -> getTextureCache()->getCachedTextureInfo().c_str());
     log("%s\n", SpriteFrameCache::getInstance()->getCachedSpriteFrameInfo().c_str());
-//    char tlist_1[1024] = {0},tlist_2[1024]={0};
-//    char fname[7][8] = {"a1","b1","c1","d1","e1","f1","g1"};
-//    int i = 0, len_1,len_2 = 0;
-//    
-//    len_1 = snprintf(tlist_1,1024,"%s;",fname[0]);
-//    len_2 = snprintf(tlist_2,1024,"%s;",fname[0]);
-//    
-//    for(i=1;i<7;i++)
-//    {
-//        len_1 = snprintf(tlist_1,1024,"%s%s;",tlist_1,fname[i]);
-//        len_2 = sprintf(tlist_2,"%s%s;",tlist_2,fname[i]);
-//    }
-//    
-//    printf("tlist_1: %s\n",tlist_1);
-//    printf("tlist_2: %s\n",tlist_2);
-
 
 }
 
